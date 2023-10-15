835531f36dfcea4369104ee06c67007256b8d212
janithgunawardana1998 <janithgunawardana98@gmail.com>
2023-10-15 17:30:44 +0530

    Ui Chnages

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { themeColors } from '../theme'
@@ -16,33 +16,68 @@ export default function WelcomeScreen() {
         '⚫Hailstorms',
         '⚫Tornadoes',
         '⚫Wildfires',
-      ];
-  return (
-    <SafeAreaView className="flex-1" style={{backgroundColor: themeColors.bg}}>
-        <View className="flex-1 flex justify-around my-4">
-            <Text 
-                className="text-white font-bold text-4xl text-center">
-               Alert Me
-            </Text>
-         
-            <View className="space-y-4">
-                <TouchableOpacity
-                    onPress={()=> navigation.navigate('SignUp')}
-                    className="py-3 bg-yellow-400 mx-7 rounded-xl">
-                        <Text 
+    ];
+    return (
+        <SafeAreaView className="flex-1" style={{ backgroundColor: themeColors.bg }}>
+            <View className="flex-1 flex justify-around my-4">
+                <Text
+                    className="text-white font-bold text-4xl text-center">
+                    Alert Me
+                </Text>
+                {/* <Text style={styles.earlywarning}>
+        The AlertMe app will provide you Real-time early warnings
+        disasters in Sri Lanka.
+      </Text>
+      <View>
+        {naturalDisasters.map((disaster, index) => (
+          <Text key={index} style={styles.earlywarning2}>
+            {disaster}
+          </Text>
+        ))}
+      </View> */}
+                <Text style={styles.earlywarning}>"Alert-Me" is a powerful app for instant communication of critical information,
+                    ensuring the safety of individuals in emergencies.
+                    Quickly share vital alerts to keep those under your care safe.</Text>
+
+                <View className="space-y-4">
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('SignUp')}
+                        className="py-3 bg-yellow-400 mx-7 rounded-xl">
+                        <Text
                             className="text-xl font-bold text-center text-gray-700"
                         >
                             Sign Up
                         </Text>
-                </TouchableOpacity>
-                <View className="flex-row justify-center">
-                    <Text className="text-white font-semibold">Already have an account?</Text>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Login')}>
-                        <Text className="font-semibold text-yellow-400"> Log In</Text>
                     </TouchableOpacity>
+                    <View className="flex-row justify-center">
+                        <Text className="text-white font-semibold" style={styles.Login}>Already have an account?</Text>
+                        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                            <Text className="font-semibold text-yellow-400" style={styles.Login}> Log In</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
+
 }
+const styles = StyleSheet.create({
+
+    earlywarning: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff8dc',
+        padding: 9,
+    },
+    earlywarning2: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+        paddingRight: -50,
+        alignContent: 'center',
+        paddingRight: 20,
+    },
+    Login: {
+        fontSize: 18,
+    }
+})
