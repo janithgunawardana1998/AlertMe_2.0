a1613d96facd6358face186ad90f517ecca7276b
janithgunawardana1998 <janithgunawardana98@gmail.com>
2023-10-30 01:25:01 +0530

    Add DateTime and Dismiss Button

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -1,45 +1,77 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Card, Title, Paragraph, Button } from 'react-native-paper';
+
+const CardScreen = () => {
+    const [isCardVisible, setCardVisible] = useState(true);
+
+    const Dissmiss = () => {
+        setCardVisible(false);
+    };
+
+    const [date, setDate] = useState(null);
+    const [dateTime, setDateTime] = useState(null);
+
+    useEffect(() => {
+        let today = new Date();
+        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        setDate(date);
+        let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+        setDateTime(date + ' ' + time);
+    }, []);
 
 
-const CardScreen = () => (
-    <Card style={styles.card}>
-        <Card.Content>
+    return isCardVisible ? (
+        <Card style={styles.card}>
+            <Card.Content>
+                <Title style={styles.cardTitle}>Emergency Alert !</Title>
+                <Paragraph style={styles.cardContent}>Flood Warning in Gampaha</Paragraph>
 
-            <Title style={styles.cardTitle}>Emergency Alert</Title>
-            <Paragraph style={styles.cardContent}>Flood Warning in Gampaha</Paragraph>
-        </Card.Content>
-        <Card.Cover
-            source={{ uri: 'https://picsum.photos/700' }}
-            style={styles.cardCover}
-        />
-        <Card.Actions style={styles.cardActions}>
-            <Button>Dissmiss</Button>
-        </Card.Actions>
-    </Card>
-);
+            </Card.Content>
+            <Card.Cover
+                source={{ uri: 'https://picsum.photos/700' }}
+                style={styles.cardCover}
+            />
+            <Card.Actions style={styles.cardActions}>
+
+                <Text style={styles.paragraph}> {dateTime}</Text>
+                <Button style={styles.button} onPress={Dissmiss}>
+                    Dismiss
+                </Button>
+            </Card.Actions>
+        </Card>
+    ) : null;
+};
 
 const styles = StyleSheet.create({
     card: {
-        margin: 10,
+        margin: 16,
     },
     cardTitle: {
-        fontSize: 25,
-        marginBottom: 5,
+        fontSize: 24,
         fontWeight: 'bold',
-        color: 'red',
+
     },
     cardContent: {
-        fontSize: 14,
-        fontWeight: 'bold',
+        fontSize: 18,
     },
     cardCover: {
-        height: 150,
+        height: 200,
     },
     cardActions: {
         justifyContent: 'flex-end',
     },
+    paragraph: {
+        margin: 4,
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'left',
+
+    },
+    button: {
+        color: 'red'
+    }
+
 });
 
 export default CardScreen;
